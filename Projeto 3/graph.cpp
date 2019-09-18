241cddae9cceb1c2298cb47fa967fa91e06abce8
Teitei011 <teiteix@DESKTOP-7VCJOOR.localdomain>
2019-09-18 09:59:39 -0300

    Now it read the numbers that you put

--- Projeto 3/graph.cpp
+++ Projeto 3/graph.cpp
@@ -1,53 +1,77 @@
 #include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <list>
 #include <random>
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include <list>
 
 namespace chrono = std::chrono;
+std::string read_argument(int argc, char *argv[]);
 
-class Graph
+class Graph {
   std::vector<int> *adj;
   int max_number{1};
-  {
+
 public:
-    Graph();
-    void addEdge(int origem, int destino);
-    void show_graph():
+  Graph();
+  void addEdge(int origem, int destino);
+  void show_graph();
 };
 
-Graph:Graph(){
-  std::vector<int> *adj = new std::vector<int>[5];
+Graph::Graph() { std::vector<int> *adj = new std::vector<int>[5]; }
+
+void Graph::addEdge(int origem, int destino) {
+  adj[origem].push_back(destino);
+
+  if (adj.back() >
+      destino) { // Caso o valor de acesso ultrapasse o que tem no vector
+    // Verifando agora qual é o maior numero que contém na lista
+  } else {
+    adj[destino].push_back(origem);
+  }
 }
 
-void Graph::addEdge(int origem, int destino)
-{
-    adj[origem].push_back(destino);
+std::string read_argument(int argc, char *argv[]) {
+  std::string filename;
 
-    if(destino ){
-      adj[destino].push_back(origem);
+  // We need exactly three arguments (considering program name).
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <filename>\n";
+    exit(1);
+  }
+
+  filename = argv[1];
+  return filename;
+}
+
+int main(int argc, char *argv[]) {
+
+  std::string filename = read_argument(argc, argv);
+  std::cout << "The filename is: " << filename << '\n';
+
+  std::vector<int> numbers;
+  int higher_number{0};
+
+  fstream input(
+      filename); // put your program together with thsi file in the same folder.
+  if (input.is_open()) {
+    while (!input.eof()) {
+      // if ()
+      getline(input, number);      // read number
+      data = atoi(number.c_str()); // convert to integer
+      cout << data << endl;        // print it out
     }
-}
+  }
 
-void Graph::show_graph(){
-}
+  // Graph g();
+  // g.addEdge(0, 1);
+  // g.addEdge(0, 2);
+  // g.addEdge(1, 2);
+  // g.addEdge(2, 3);
+  //
+  // g.show_graph();
 
-int read_data(){
-
-}
-
-int main()
-{
-    Graph g();
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 3);
-
-    g.show_graph();
-
-    return 0;
+  return 0;
 }
---
+++ Projeto 3/graph_lists.cpp
@@ -0,0 +1,97 @@
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace chrono = std::chrono;
+std::string read_argument(int argc, char *argv[]);
+
+class Graph {
+  std::list<int> adj;
+
+public:
+  Graph();
+  void show_graph();
+  void find_triangle();
+  void addEdge(int origem, int destino);
+};
+
+void Graph::Graph() {}
+
+void Graph::show_graph() {
+  std::cout << "Graph contains: ";
+  for (auto it = std::begin(this.adj); it != std::end(this.adj); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+}
+
+void Graph::find_triangle(){
+
+}
+
+
+void Graph::addEdge(int origem, int destino) {
+  adj.insert(origem, destino);
+  adj.insert(destino, origem);
+}
+
+std::string read_argument(int argc, char *argv[]) {
+  std::string filename;
+
+  // We need exactly three arguments (considering program name).
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <filename>\n";
+    exit(1);
+  }
+
+  filename = argv[1];
+  return filename;
+}
+
+
+int main(int argc, char *argv[]) {
+
+  std::string filename = read_argument(argc, argv);
+  std::cout << "The filename is: " << filename << '\n';
+
+  std::ifstream infile;
+  infile.open(filename);
+
+  if (infile.is_open())
+  {
+      int num;
+      while(infile >> num)
+      {
+          std::cout << num << '\n';
+      }
+  }
+
+  // The time monitor
+  double elapsed = 0;
+  chrono::high_resolution_clock::time_point t1, t2;
+
+  t1 = chrono::high_resolution_clock::now();
+
+  // Graph g();
+  // g.addEdge(0, 1);
+  // g.addEdge(0, 2);
+  // g.addEdge(1, 2);
+  // g.addEdge(2, 3);
+  // //
+
+  t2 = chrono::high_resolution_clock::now();
+
+  auto dt = chrono::duration_cast<chrono::microseconds>(t2 - t1);
+
+  // Show timing results
+  // std::cout << "Time taken: " << dt  << std::endl;
+
+  // g.show_graph();
+
+  return 0;
+}
