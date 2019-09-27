6bb916e2cbf99e22d9230975522a498c19b892e5
10414866-stefantcleal@usp.br <stefantcleal@turmalina4.lef>
2019-09-27 11:32:14 -0300

    Working

--- Prática 3/mpi.cpp
+++ Prática 3/mpi.cpp
@@ -9,6 +9,25 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &quantos);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  if (quantos %2 == 0){ // Condicao par
+
+    if (rank < quantos / 2){
+      std::cout << "O processo " << rank << " esta entre os primeiros." << '\n';
+    }else{
+      std::cout << "O processo " << rank << "esta entre os ultimos." << '\n';
+    }
+
+  } else{ // COndicao impar
+    if(rank < (quantos - 1) / 2){
+      std::cout << "O processo " << rank << "esta entre os primeiros." << '\n';
+    } else if(rank != quantos){
+      std::cout << "O processo " << rank << "esta entre os ultimos." << '\n';
+    }
+    else{
+      std::cout << "O processo " << " e o lanterninha." << '\n';
+    }
+  }
+
   std::cout << "Processo " << rank << " de " << quantos << " rodando."
             << std::endl;
 
