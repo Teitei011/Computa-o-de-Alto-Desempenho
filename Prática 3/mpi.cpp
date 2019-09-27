5a43f9ae8ca6df1be8cdf2370151b3b51c2123ac
10414866-stefantcleal@usp.br <stefantcleal@turmalina4.lef>
2019-09-27 11:26:35 -0300

    Acho que terminei, falta perguntar pro professor

--- Prática 3/mpi.cpp
+++ Prática 3/mpi.cpp
@@ -9,8 +9,26 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &quantos);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  std::cout << "Processo " << rank << " de " << quantos << " rodando."
-            << std::endl;
+  if (quantos %2 == 0){ // Condicao par
+
+    if (rank < quantos / 2){
+      std::cout << "O processo " << rank << " esta entre os primeiros." << '\n';
+    }else{
+      std::cout << "O processo " << rank << " esta entre os ultimos." << '\n';
+    }
+
+  } else{ // Condicao impar
+    if(rank < (quantos - 1) / 2){
+      std::cout << "O processo " << rank << " esta entre os primeiros." << '\n';
+    } else if(rank == quantos - 1){
+      std::cout << "O processo " << rank << " e o lanterninha." << '\n';
+    }
+    else{
+      std::cout << "O processo " << rank << " esta entre os ultimos." << '\n';
+    }
+  }
+
+  // std::cout << "Processo " << rank << " de " << quantos << " rodando." << std::endl;
 
   MPI_Finalize();
   return 0;
